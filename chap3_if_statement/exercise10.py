f94274c4c9c97f0cef6bf7e4e85285a8ab310c1e
shinks <adf5159@gmail.com>
2023-12-03 17:04:37 +0900

    Chap.3 If Statement ex.10 ~ ex.13

---
+++ chap3_if_statement/exercise10.py
@@ -0,0 +1,4 @@
+num = int(input("Enter a number: "))
+
+if num > 5:
+    print(f"{num} is bigger than 5")
---
+++ chap3_if_statement/exercise13.py
@@ -0,0 +1,5 @@
+num = int(input("Enter a score: "))
+threshold = 80
+
+if num > threshold:
+    print(f"Pass!")
