73e80429d1437696bdeed719ae28447d5b595c73
shinks <adf5159@gmail.com>
2023-12-03 15:45:13 +0900

    Chap.1 Boolean Values ex.1 ~ ex.5

---
+++ chap1_boolean_values/exercise4.py
@@ -0,0 +1,8 @@
+num1, num2 = 10, 5
+
+print(num1 > num2)
+print(num1 >= num2)
+print(num1 < num2)
+print(num1 <= num2)
+print(num1 == num2)
+print(num1 != num2)
---
+++ chap1_boolean_values/exercise5.py
@@ -0,0 +1,6 @@
+test_list = []
+print(len(test_list) == 0)
+
+test_list = [1, 2, 3]
+print(len(test_list) > 5)
+print(len(test_list) < 10)
